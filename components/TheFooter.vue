172f2f68245a6200b7d96c9a618ec0f1dbfbb7ee
Unai Mengual <hey@owln.ai>
2022-02-17 22:16:47 +0100

    chore(footer): more accurate tagline

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,11 +3,11 @@
 		class="text-xs sm:text-sm xl:text-base uppercase font-medium text-gray-500 dark:text-gray-400 px-8 leading-relaxed">
 		<p>*If no module is found at the moment, it will be made in time.</p>
 		<p class="md:fixed top-0 right-0 p-8">
-			A
+			An
 			<a
 				href="https://github.com/owlnai/the-nuxt-rule"
 				class="underline underline-offset-2"
-				>weekend idea</a
+				>afternoon project</a
 			>
 			by
 			<a href="https://twitter.com/owln_ai" class="underline underline-offset-2"
